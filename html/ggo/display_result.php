49009ad0cea1dfafd6f910cc44f5d58633da0481
Chris Maloof <cjmaloof@gmail.com>
2021-01-20 03:57:16 +0000

    Reuse result display Ajax code

--- html/ggo/display_result.php
+++ html/ggo/display_result.php
@@ -3,6 +3,7 @@
 <head>
   <title>Game Group Organizer - Results</title>
   <link rel="stylesheet" href="game_ranker.css">
+  <script src="reqwest.min.js"></script>
 </head>
 <body>
 
@@ -16,28 +17,26 @@ $rank_string = $mysqli->real_escape_string($_POST['ranks']);
 $submitted_player = $ordinal - 1;
 insertRanks($mysqli, $session_label, $submitted_player, $rank_string);
 
-$session_id = fetchSessionId($mysqli, $session_label);
-
-$input = fetchRanksByPlayer($mysqli, $session_id);
-foreach ($input as $player) {
-    echo "<p><b>$player[0]</b>: ";
-    foreach (array_slice($player, 1, max(0, count($player)-2)) as $games) {
-        echo joinSameRankGames($games) . "; then ";
-    }
-    echo joinSameRankGames(end($player)) . ".";
-}
-
-$output = calculateRanks($session_id);
-echo "<p>$output</p>";
-
-function joinSameRankGames($array) {
-    $cssArray = array_map(function($s) { return "<span class=\"gamename\">$s</span>"; }, $array);
-    if (count($cssArray) < 3) {
-        return join(" or ", $cssArray);
-    } else {
-        return join(", ", array_slice($cssArray, 0, count($cssArray)-1)) . ", or " . end($cssArray);
-    }
-}
+echo "<div id=\"playerRanks\"></div>";
+echo "<div id=\"results\"></div>";
 ?>
+
+<script>
+reqwest({
+    url: 'player_ranks.php',
+    method: 'get',
+    data: { session: '<? echo "$session_label"; ?>' }
+}).then(function(response) {
+    document.getElementById('playerRanks').innerHTML = response;
+});
+
+reqwest({
+    url: 'calculated_result.php',
+    method: 'get',
+    data: { session: '<? echo "$session_label"; ?>' }
+}).then(function (response) {
+    document.getElementById('results').innerHTML = response;
+});
+</script>
 
 <?php endDocument(); ?>
