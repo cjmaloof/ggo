9522135ab3c1740b91c00b8d88994f5cda421837
Chris Maloof <cjmaloof@gmail.com>
2021-02-09 03:48:33 +0000

    Minimal index.php

---
+++ html/ggo/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>What Do We Play?</title>
+  <link rel="stylesheet" href="game_ranker.css">
+</head>
+
+<?php require 'imports.php'; ?>
+
+<body>
+<p><a href="create_single">Create a group with one shared device</a></p>
+<p><a href="create">Create a group where everyone has a device</a></p>
+<p><a href="join">Join a group</a></p>
+</body>
+</html>
+
+<?php endDocument(); ?>
