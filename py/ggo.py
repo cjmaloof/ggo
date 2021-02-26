bfda08dc7520587eff30ebd14a96529a860afb59
Chris Maloof <cjmaloof@gmail.com>
2021-02-26 22:07:28 +0000

    Handle all players playing the same game

--- py/ggo.py
+++ py/ggo.py
@@ -22,51 +22,64 @@ class GameGroup:
         
     def htmlString(self, playerNames, gameNames):
         result = ""
-        for (group, game) in zip(self.groups, self.games):
-            for player in group:
+        if self.games[0] == self.games[1]:
+            for player in self.groups[0] + self.groups[1]:
                 result += cgi.escape(playerNames[player], True) + ", "
-            result += " play " + cgi.escape(gameNames[game], True) + "<br />"
+            result += " all play " + cgi.escape(gameNames[self.games[0]], True) + "<br />"
+        else:
+            for (group, game) in zip(self.groups, self.games):
+                for player in group:
+                    result += cgi.escape(playerNames[player], True) + ", "
+                result += " play " + cgi.escape(gameNames[game], True) + "<br />"
         return result + "<br />"
 
 def rank(dbData):
-    playersExceptFirst = range(1, dbData.playerCount)
-    group1Options = itertools.combinations(playersExceptFirst, (dbData.playerCount / 2) - 1)
-    if (dbData.playerCount % 2 == 1):
-        group1Options = itertools.chain(group1Options, itertools.combinations(playersExceptFirst, (dbData.playerCount + 1) / 2 - 1))
-    # group1 plus player 0 yields all possibilities for game 1 players.
-    
     gamePairs = list(itertools.product(range(0, dbData.gameCount), repeat=2))
     
     result = ""
     scoreToGameGroups = {}
-    playersExceptFirstSet = frozenset(playersExceptFirst)
-    for group1 in group1Options:
-        group1 = [0] + list(group1)
-        group2 = tuple(playersExceptFirstSet.difference(group1))
-        for pair in gamePairs:
+            
+    for pair in gamePairs:
+        for (group1, group2) in playerCombinationsForTwoGames(dbData.playerCount):
             groupScore = score(GameGroup(pair, [group1, group2]), dbData.penalties, dbData.gameCount)
             if groupScore not in scoreToGameGroups:
                 scoreToGameGroups[groupScore] = []
             scoreToGameGroups[groupScore].append(GameGroup(pair, (group1, group2)))
-    
+            
+            # If the games are the same, all groups will score the same.
+            if pair[0] == pair[1]:
+                break
+            
     sortedScores = sorted(scoreToGameGroups.keys())
     
-    for index, adjective in enumerate(["Best", "Second-best", "Third-best"]):
-        result += adjective + " score: " + str(normalize(sortedScores[index], dbData.playerCount)) + "<br />"
-        for gameGroup in scoreToGameGroups[sortedScores[index]]:
+    for topScore, adjective in zip(sortedScores[:3], ["Best", "Second-best", "Third-best"]):
+        result += adjective + " score: " + str(normalize(topScore, dbData.playerCount)) + "<br />"
+        for gameGroup in scoreToGameGroups[topScore]:
             result += gameGroup.htmlString(dbData.playerNames, dbData.gameNames)
         
     return result
 
+# Return an iterator over pairs of tuples
+def playerCombinationsForTwoGames(playerCount):
+    playersExceptFirst = range(1, playerCount)
+    group1Options = itertools.combinations(playersExceptFirst, (playerCount / 2) - 1)
+    if (dbData.playerCount % 2 == 1):
+        group1Options = itertools.chain(group1Options, itertools.combinations(playersExceptFirst, (playerCount + 1) / 2 - 1))
+    # group1 plus player 0 yields all possibilities for game 1 players.
+    playersExceptFirstSet = frozenset(playersExceptFirst)
+    return itertools.imap(lambda group1: ([0] + list(group1), list(playersExceptFirstSet.difference(group1))), group1Options)
+
+# Compute the score for one set of groups matched with games
 def score(gameGroup, penalties, gameCount):
     score = 0
-    for (group, game) in zip(gameGroup.groups, gameGroup.games):
-        for player in group:
-            try:
-                score += penalties[(player * gameCount) + game]
-            except IndexError:
-                print("Error on player="+str(player)+", gameCount="+str(gameCount)+", game="+str(game))
-                raise
+    for group, game in zip(gameGroup.groups, gameGroup.games):
+        score += scoreOneGame(group, game, penalties, gameCount)
+    return score
+
+def scoreOneGame(group, game, penalties, gameCount):
+    score = 0
+    for player in group:
+        score += penalties[(player * gameCount) + game]
     return score
 
 # Normalizes scores such that a first-place choice is 0 and a second-place choice is 1
@@ -86,12 +99,6 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect(host=server, user=user, passwd=password, db=dbName)
     cursor = db.cursor()
-
-    #cursor.execute("SELECT count(*) FROM player WHERE session_id=%s", sessionId)
-    #playerCount = cursor.fetchone()[0];
-    
-    #cursor.execute("SELECT count(*) FROM game WHERE session_id=%s", sessionId)
-    #gameCount = cursor.fetchone()[0];
     
     cursor.execute("SELECT name FROM player WHERE session_id=%s ORDER BY ordinal", sessionId)
     playerNamesData = map(lambda t: t[0], cursor.fetchall())
